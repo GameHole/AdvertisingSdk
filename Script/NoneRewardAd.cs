ce09bdd5d2ce903b79b1c512d91f3dabf7eb2331
BlackHole <38195607+GameHole@users.noreply.github.com>
2020-10-15 20:32:28 +0800

    提交

---
+++ Script/NoneRewardAd.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace MiniGameSDK
+{
+    public class NoneRewardAd : IRewardAdAPI
+    {
+        public bool isNotUseAd { get; set; }
+        public Action<bool> onClose { get; set; }
+
+        public void AutoShow(Action<bool> onclose)
+        {
+            onclose?.Invoke(true);
+        }
+
+        public Task<bool> AutoShowAsync()
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+            tcs.SetResult(true);
+            return tcs.Task;
+        }
+
+        public bool isReady()
+        {
+            return true;
+        }
+
+        public void Show()
+        {
+            onClose?.Invoke(true);
+        }
+    }
+}
