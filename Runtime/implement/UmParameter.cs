98bf2db16cdc30195bc5e86f729fe87d7f41a063
GameHole <741419087@qq.com>
2020-12-05 10:37:56 +0800

    调整

---
+++ Runtime/implement/UmParameter.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniGameSDK
+{
+    public class UmParameter : AScriptableObject
+    {
+        public override string filePath => "友盟参数/Parameter";
+        public string appid;
+        public string channal;
+        public bool debug;
+    }
+}
