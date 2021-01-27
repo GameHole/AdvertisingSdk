7ce57e09eff14f514df7e640f3303ebd02067fb7
GameHole <741419087@qq.com>
2021-01-27 10:52:10 +0800

    修改BUG

---
+++ Scripts/BannerReShower.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MiniGameSDK
+{
+	public class BannerReShower:MonoBehaviour
+	{
+        IBannerAd banner;
+        public float reShowTime = 30;
+        float add;
+        bool isRun;
+        public bool useReShow;
+        private void Start()
+        {
+            banner.onClose += (v) =>
+            {
+                StartCountDown();
+            };
+            banner.onHide += () =>
+            {
+                StopCountDown();
+            };
+        }
+        public void StartCountDown()
+        {
+            add = 0;
+            isRun = useReShow;
+        }
+        public void StopCountDown()
+        {
+            isRun = false;
+        }
+        private void Update()
+        {
+            if (isRun)
+            {
+                add += Time.deltaTime;
+                if (add >= reShowTime)
+                {
+                    add = 0;
+                    isRun = false;
+                    if (useReShow)
+                    {
+                        banner?.Show();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/ISplashAd.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MiniGameSDK
+{
+	public interface ISplashAd:IInterface
+	{
+        void Show();
+        Action OnClsoe { get; set; }
+	}
+}
